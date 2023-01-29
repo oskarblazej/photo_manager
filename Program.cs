4e5e6341b629a1262c4d0ca2361ef2b315c6e5e0
Oskar Hampel <hampeloskarmt@gmail.com>
2023-01-29 20:05:02 +0100

    Create moving files functionality with menu

--- Program.cs
+++ Program.cs
@@ -1,63 +1,25 @@
 ﻿using System;
 using System.IO;
+using static System.Net.WebRequestMethods;
 
 namespace PhotoManager
 {
     internal class Program
     {
-        
+        public static string path = Directory.GetCurrentDirectory();
+
         static void Main(string[] args)
         {
-            string path = Directory.GetCurrentDirectory();
             DirectoryInfo d = new DirectoryInfo(path); //Assuming Test is your Folder
             Console.Write("Please provide file extension ex: '.jpg': ");
             string FileExt = "*" + Console.ReadLine();
-            FileInfo[] Files = d.GetFiles(FileExt); //Getting Text files
-           
-            foreach (FileInfo file in Files)
-            {
-                Console.WriteLine(file);
-                // get the creation date and then try to get year of the file creation
-                // after that create a variable to store the path for creating a folder
-                string created = Convert.ToString(file.CreationTime);
-                string year = created.Substring(6, 4);
-                string year_path = Path.Combine(path, year);
-                try
-                {
-                    if (Directory.Exists(year_path))
-                    {
-                        // Try to create the directory.
-                        Console.WriteLine("Directory exists");
-                        
-                        
-                    }
-                    else
-                    {
-                        Console.WriteLine("Directory does not exist");
-                        DirectoryInfo di = Directory.CreateDirectory(year_path);
-                    }
-                    try
-                    {
-                        File.Move(file.Name, year_path+"\\"+file.Name);
-                        Console.WriteLine("Moved");
-                    }
-                    catch (Exception e)
-                    { 
-                        Console.WriteLine("File could not be moved");
-                        Console.WriteLine(e.Message);
-                    }
-                    
-                }
-                catch (IOException ioex)
-                {
-                    Console.WriteLine(ioex.Message);
-
-                }
-            }
-            Console.ReadKey();
-            /*
-             * 
+            FileInfo[] Files = d.GetFiles(FileExt); //Getting Text files    
+         
+            ///*
             int choice ;
+            bool sYear = false;
+            bool sMonth = false;
+            bool sDay = false;
             Console.WriteLine("---------Menu---------");
             Console.WriteLine("1. Sort by creation year");
             Console.WriteLine("2. Sort by creation year and month");
@@ -77,9 +39,18 @@ namespace PhotoManager
                 }
             switch (choice)
             {
-                case 1: break; 
-                case 2: break; 
-                case 3: break; 
+                case 1:
+                    sYear = true;
+                    break; 
+                case 2:
+                    sYear = true;
+                    sMonth = true;
+                    break; 
+                case 3:
+                    sYear = true;
+                    sMonth = true;
+                    sDay = true;
+                    break; 
                 case 4:
                     Console.WriteLine("Thank you for using our application!");
                     System.Environment.Exit(0);
@@ -88,10 +59,66 @@ namespace PhotoManager
                     Console.WriteLine("Option not listed in the meny");
                     goto start;
             }
-            */
+            SortFiles(Files, sYear, sMonth, sDay);
+            //*/
+        }
 
 
+        public static void SortFiles(FileInfo[] files, bool sYear, bool sMonth, bool sDay)
+        {
+            int counter = 0;
+            string folder_path;
+            string date = "";
+            foreach (FileInfo file in files)
+            {
+                // get the creation date and then try to get year of the file creation
+                // after that create a variable to store the path for creating a folder
+                string created = Convert.ToString(file.CreationTime);
+                // if sDay is true means the rest is also true, less typing
+                if (sDay == true)
+                {
+                    date = created.Substring(6, 4) + "\\" + created.Substring(3, 2) + "\\" + created.Substring(0, 2);
+                    folder_path = Path.Combine(path, date);
+                }
+                else if (sMonth == true)
+                {
+                    date = created.Substring(6, 4) + "\\" + created.Substring(3, 2);
+                    folder_path = Path.Combine(path, date);
 
+
+                }
+                else
+                {
+                    date = created.Substring(6, 4);
+                    folder_path = Path.Combine(path, date);
+                }
+                
+                try
+                {
+                    if (!Directory.Exists(folder_path))
+                    {
+                        DirectoryInfo di = Directory.CreateDirectory(folder_path);
+                    }
+                    System.IO.File.Move(file.Name, folder_path + "\\" + file.Name);
+                    
+                }
+                catch (IOException ioex)
+                {
+                    Console.WriteLine(ioex.Message);
+                    counter++;
+
+                }
+            }
+            if (counter == 0)
+            {
+
+                Console.WriteLine("\nAll files were moved succesfully. Thank you for using our program!");
+            }
+            else
+            {
+                Console.WriteLine(counter + " errors occured. Check the progress in appropriate directory.");
+            }
+            
         }
     }
 }
